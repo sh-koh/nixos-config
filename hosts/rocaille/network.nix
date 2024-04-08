4b62da9ef5de980ee59a48067f0c3157eda599ca
sh-koh <abdel.briand.a@gmail.com>
2024-04-08 13:59:16 +0200

    [🔨] fix(hosts/rocaille): added eni vpn

--- hosts/rocaille/network.nix
+++ hosts/rocaille/network.nix
@@ -16,6 +16,14 @@
     enable = true;
   };
 
+  services.openvpn.servers.eni = {
+    updateResolvConf = true;
+    autoStart = false;
+    config = "config ${config.age.secrets.vpn-eni-cfg.path}";
+    up = config.age.secrets.vpn-eni-up.path;
+    down = "${pkgs.openresolv}/sbin/resolvconf -d tun0";
+  };
+
   services = {
     printing = {
       enable = true;
