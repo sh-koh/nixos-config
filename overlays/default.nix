fe4e48f8b3dbb11cfada531b715f146ab0b3076c
sh-koh <abdel.briand.a@gmail.com>
2024-04-13 15:45:26 +0200

    [✏️] norm(overlays/vesktop): use the hardware-accel draft

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,6 +5,14 @@
     nerdfonts = prev.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
     wlroots = prev.wlroots_0_17.overrideAttrs (oldAttrs: { patches = ./patches/wlroots-nvidia.patch; });
     prismlauncher = prev.prismlauncher.override { withWaylandGLFW = true; };
+    vesktop = prev.vesktop.overrideAttrs (oldAttrs: {
+      src = final.fetchFromGitHub {
+        owner = "kaitlynkittyy";
+        repo = "Vesktop";
+        rev = "d0ff6192cbf81f1a0b497e16dff9ac7c23616436";
+        hash = "sha256-/qd++djD5LQaDzxslAA55A6jZHCT6NLQ+RYofqn1x28=";
+      };
+    });
   };
 
   master = final: _prev: {
