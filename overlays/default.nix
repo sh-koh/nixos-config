2d3733d1b8372e3ea7b0853777e05e6f08c92ffc
sh-koh <abdel.briand.a@gmail.com>
2024-04-21 19:51:55 +0200

    [🔨] fix(overlay): clean up

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,16 +3,7 @@
 
   modifications = final: prev: {
     nerdfonts = prev.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
-    wlroots = prev.wlroots_0_17.overrideAttrs (oldAttrs: { patches = ./patches/wlroots-nvidia.patch; });
     prismlauncher = prev.prismlauncher.override { withWaylandGLFW = true; };
-    vesktop = prev.vesktop.overrideAttrs (oldAttrs: {
-      src = final.fetchFromGitHub {
-        owner = "kaitlynkittyy";
-        repo = "Vesktop";
-        rev = "11f9970f0ce22798b6c53f88565de8bb044798c8";
-        hash = "sha256-+jxJQNwqYmgjUXGSW3CqP+XIdYlgXBKAyrevQSttFvc=";
-      };
-    });
   };
 
   master = final: _prev: {
