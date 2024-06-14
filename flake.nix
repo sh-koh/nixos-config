5c68ca890cdd0f8611d86460e3b9514de2345aa9
sh-koh <abdel.briand.a@gmail.com>
2024-06-14 14:38:22 +0200

    「🔨」 fix(flake): remove umu and update!

--- flake.nix
+++ flake.nix
@@ -71,14 +71,13 @@
       ref = "v0.40.0";
       inputs.hyprland.follows = "hyprland";
     };
-    umu = {
-      type = "git";
-      url = "https://github.com/Open-Wine-Components/umu-launcher";
-      dir = "packaging/nix";
-      ref = "nix";
-      submodules = true;
-      inputs.nixpkgs.follows = "unstable";
-    };
+    # umu = {
+    #   type = "git";
+    #   url = "https://github.com/Open-Wine-Components/umu-launcher";
+    #   dir = "packaging/nix";
+    #   submodules = true;
+    #   inputs.nixpkgs.follows = "unstable";
+    # };
     agenix = {
       type = "github";
       owner = "ryantm";
