408e00ac8a8a26a133fdf18dbdfab946fbb4d823
sh-koh <abdel.briand.a@gmail.com>
2024-06-15 15:43:11 +0200

    「✨」 feat(lutris,umu): lutris -> 0.5.17, made lutris use umu

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,6 +4,21 @@
   modifications = final: prev: {
     nerdfonts = prev.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
     prismlauncher = prev.prismlauncher.override { withWaylandGLFW = true; };
+    btop = prev.btop.override { cudaSupport = true; };
+    lutris = prev.lutris.override { extraPkgs = pkgs: [ inputs.umu.packages.${final.system}.umu ]; };
+    lutris-unwrapped = prev.lutris-unwrapped.overrideAttrs {
+      version = "0.5.17";
+      src = prev.fetchFromGitHub {
+        owner = "lutris";
+        repo = "lutris";
+        rev = "v0.5.17";
+        hash = "sha256-Tr5k5LU0s75+1B17oK8tlgA6SlS1SHyyLS6UBKadUmw=";
+      };
+      postPatch = ''
+        substituteInPlace lutris/util/magic.py \
+          --replace '"libmagic.so.1"' "'${prev.lib.getLib prev.file}/lib/libmagic.so.1'"
+      '';
+    };
   };
 
   stable = final: _prev: {
