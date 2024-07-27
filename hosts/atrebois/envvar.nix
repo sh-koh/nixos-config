a05d283da6b3736effc1533ed5167fb3466a532a
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-07-27 13:17:53 +0200

    「✨」 feat(hosts/atrebois,rocaille): switch from french to english

--- hosts/atrebois/envvar.nix
+++ hosts/atrebois/envvar.nix
@@ -29,20 +29,20 @@
     KITTY_ENABLE_WAYLAND = "1";
   };
 
-  i18n.defaultLocale = "fr_FR.UTF-8";
-  i18n.supportedLocales = [ "fr_FR.UTF-8/UTF-8" ];
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-    LC_ALL = "fr_FR.UTF-8";
-    LANG = "fr_FR.UTF-8";
-    LANGUAGE = "fr_FR";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LANG = "en_US.UTF-8";
+    LANGUAGE = "en_US";
   };
 }
