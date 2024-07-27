901968cc00fe4f48b9e71eaf849118a42557618d
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-07-27 16:35:16 +0200

    「✨」 feat(pkgs): added hyprsplit and hyprXPrimary as packages

--- pkgs/hyprXPrimary/default.nix
+++ pkgs/hyprXPrimary/default.nix
@@ -15,15 +15,17 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "69388ceb830ba6c68496f928dec173e578d6fd14";
     hash = "sha256-ltRLHOpgYxtm7EfvikDgLPiG0AiJaAFIHwGdLe9aoNE=";
   };
+
   nativeBuildInputs = [
     pkg-config
   ];
+
   buildInputs = [
     pixman
     libdrm 
     hyprland
-    hyprland.buildInputs
-  ];
+  ] ++ hyprland.buildInputs;
+
   installPhase = ''
     mkdir -p $out/lib 
     cp hyprXPrimary.so $out/lib/libhyprxprimary.so
--- pkgs/hyprsplit/default.nix
+++ pkgs/hyprsplit/default.nix
@@ -14,8 +14,10 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "shezdy";
     repo = "hyprsplit";
-    rev = "v0.41.1";
-    hash = "sha256-7R/Q4QRJa2mm+gAU+rJ0XGKMwXPxDq7J/iAq0fjKhKY=";
+    rev = "fcf00b770e3b89fd93de2de1bb5e68721090f5fe";
+    hash = "sha256-AMK4/87EdBcl8ukTdHMll+zbU76TySqpgwgi6J/jyks=";
+    # rev = "3051541ebc7f24dd5325cdfa458f29b4ff9648de";
+    # hash = "sha256-PxGDMJaCAm+DzbcAvw9X4RmlVOT1ApmiSc6isqWd6Oo=";
   };
 
   nativeBuildInputs = [
@@ -23,6 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
     meson
     ninja
   ];
+
   buildInputs = [
     hyprland
     pixman
