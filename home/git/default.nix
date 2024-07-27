ef7839d5f0be9a75031923941a468b80ca08a970
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-07-27 16:33:13 +0200

    「✨」 feat(git): fancy delta and gitlab

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,27 +1,36 @@
 { pkgs , ... }:
 {
+  home.packages = with pkgs; [
+    act
+    gist
+    gitflow
+  ];
+
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
     lfs.enable = true;
-
+    delta.enable = true;
     userName = "sh-koh";
     userEmail = "70974710+sh-koh@users.noreply.github.com";
-    package = pkgs.gitFull;
-
     extraConfig = {
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
       url = {
-        "ssh://git@github.com/sh-koh" = {
-          insteadOf = "https://github.com/sh-koh";
-        };
-        # "ssh://git@gitlab.com/" = {
-        #   insteadOf = "https://gitlab.com/";
-        # };
+        "ssh://git@github.com/sh-koh".insteadOf = "https://github.com/sh-koh";
+        "ssh://git@gitlab.com/sh-koh".insteadOf = "https://gitlab.com/sh-koh";
       };
+      # ignores = [
+      #   "result"
+      #   ".direnv"
+      #   "vendor"
+      #   "tmp"
+      #   "air"
+      # ];
     };
   };
+
   programs.ssh = {
     addKeysToAgent = "yes";
     matchBlocks = {
