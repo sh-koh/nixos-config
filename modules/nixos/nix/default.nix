f14e1cdc41cf179775b8152f33750c76422e9579
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-09-22 23:44:39 +0200

    「✨」 feat(nix): add nix-community's cache

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -24,6 +24,12 @@
         auto-optimise-store = true;
         builders-use-substitutes = true;
         allowed-users = [ "shakoh" ];
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
         experimental-features = [
           "auto-allocate-uids"
           "ca-derivations"
