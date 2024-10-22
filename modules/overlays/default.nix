a9c3c3d8f21bd66fd4f65768479be27605358f78
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-10-22 19:07:12 +0200

    「🔨」 fix(overlays): clean up

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -3,11 +3,8 @@
   flake.overlays = {
     additions = final: _: import ../../pkgs final.pkgs;
     modifications = final: prev:
-      withSystem prev.stdenv.hostPlatform.system ({ inputs', ... }: 
+      withSystem prev.stdenv.hostPlatform.system ({ inputs', ... }:
       {
-        nerdfonts = prev.nerdfonts.override { fonts = [ "Iosevka" ]; };
-        iosevka-bin = prev.iosevka-bin.override { variant = "SS15"; };
-        btop = prev.btop.override { cudaSupport = true; };
         lutris = prev.lutris.override { extraPkgs = _: [ inputs'.umu.packages.umu ]; };
       });
   };
