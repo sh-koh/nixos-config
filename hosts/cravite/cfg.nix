7dae9cd93f84545a1c70f5e1014f82010dd2d1b7
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-11-10 18:10:07 +0100

    「✨」 feat(cravite): enable k3s

--- hosts/cravite/cfg.nix
+++ hosts/cravite/cfg.nix
@@ -8,5 +8,10 @@ in
     kernelParams = [ "mitigations=off" "spectre_v2=off" ];
   };
 
+  networking.hostName = "cravite";
   users.users.shakoh.openssh.authorizedKeys.keys = [ atrebois rocaille ];
+
+  services.k3s = {
+    enable = true;
+  };
 }
