04eab21023ebe4d19d2db106eadd25f8f2c89838
sh-koh <70974710+sh-koh@users.noreply.github.com>
2025-07-14 15:44:07 +0200

    「✨」 feat(overlays): proton-ge-bin-10-9

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,15 +4,22 @@
     modifications =
       _final: prev:
       withSystem prev.stdenv.hostPlatform.system (
-        { ... }:
+        { pkgs, ... }:
         {
           steam = prev.steam.override { extraArgs = "-pipewire -pipewire-dmabuf -system-composer"; };
           xivlauncher-rb = prev.xivlauncher-rb.override {
-            extraExecArgs = "env SDL_VIDEODRIVER=wayland ${prev.lib.getExe prev.gamescope} -f -h 1080 -r 240 -O DP-1 --adaptive-sync --immediate-flips --force-grab-cursor --rt --ready-fd --expose-wayland --backend wayland --";
+            extraExecArgs = "env SDL_VIDEODRIVER=wayland ${prev.lib.getExe prev.gamescope} -f -h 1080 -r 240 -o 40 --adaptive-sync --immediate-flips --force-grab-cursor --force-windows-fullscreen --rt --ready-fd --expose-wayland --backend wayland --";
           };
           lutris = prev.lutris.override {
             extraPkgs = p: [ p.umu-launcher ];
           };
+          proton-ge-bin = prev.proton-ge-bin.overrideAttrs (old: rec {
+            version = "GE-Proton10-9";
+            src = pkgs.fetchzip {
+              url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
+              hash = "sha256-DJ7bRjzJehSFIyBo+oJyyWui+a3udGxc38P9Hw+xU9U=";
+            };
+          });
         }
       );
   };
