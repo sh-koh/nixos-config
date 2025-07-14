50ed4f66fd7bc4f90dfc3b66c46755b8fcd40f12
sh-koh <70974710+sh-koh@users.noreply.github.com>
2025-07-14 16:11:27 +0200

    「✨」 feat(shell): added television

---
+++ modules/home-manager/shell/television.nix
@@ -0,0 +1,16 @@
+{
+  programs.television = {
+    enable = true;
+    channels = { };
+    settings = {
+      ui = {
+        use_nerd_font_icons = true;
+        ui_scale = 120;
+        show_preview_panel = false;
+      };
+      keybindings = {
+        quit = [ "esc" ];
+      };
+    };
+  };
+}
